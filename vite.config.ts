e94fda7e63c3eed874d111710953b01dc0dd6ffa
AbrahamVelasquez <abrahamjv026@gmail.com>
2026-04-10 00:52:34 +0200

    Arreglar el viteConfig para que funcione el gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://abrahamvelasquez.github.io/karteikarten/', // ¡Ojo! Pon el nombre exacto de tu repo en GitHub
 })
