58917b8de2297b6c8d10ba8050930dd7a8733249
Abraham J. Velásquez G. <abrahamjv026@gmail.com>
2026-04-10 01:06:30 +0200

    Change base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://abrahamvelasquez.github.io/karteikarten/', // ¡Ojo! Pon el nombre exacto de tu repo en GitHub
+  base: '/karteikarten/', // ¡Ojo! Pon el nombre exacto de tu repo en GitHub
 })
